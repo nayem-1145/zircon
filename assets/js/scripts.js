625d0176f7fe10823d5870bfe5f4d5e5de3536c9
Nayem Ahmed <nayem1145@gmail.com>
2024-10-21 10:43:05 +0600

    updated

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Coupon announcement
+    document.querySelector(".zc_announce_close").addEventListener("click", function() {
+        document.querySelector(".zc_coupon_announce").classList.add("off");
+    });
+
+
+
+
+
+});
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+});
